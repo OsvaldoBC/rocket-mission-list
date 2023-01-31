b883528161762c1ede42ecbb03b8838fc1ae9249
Glenda Diaz <glendadiazah@outlook.com>
2023-01-31 16:28:15 -0600

    Style mission data as a table

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import './Mission.css';
 
 function Mission(props) {
   const missionData = props;
   return (
-    <div className="book-container">
-      <h4>{missionData.mission_id}</h4>
-      <h4>{missionData.mission_name}</h4>
-      <p>{missionData.description}</p>
+    <div className="mission-container">
+      <h4 className="name-case">{missionData.mission_name}</h4>
+      <p className="desc-case">{missionData.description}</p>
+      <h4 className="stat-case">NOT A MEMBER</h4>
+      <button className="btn-case" type="button">JOIN MISSION</button>
     </div>
   );
 }
--- src/components/Missions.js
+++ src/components/Missions.js
@@ -2,17 +2,23 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { missionList } from '../redux/missions/missions';
 import Mission from './Mission';
+import './Missions.css';
 
 function Missions() {
   const listOfMissions = useSelector((state) => state.missions);
   const send = useDispatch();
-  console.log(listOfMissions);
 
   useEffect(() => {
     send(missionList());
   }, [send]);
   return (
     <div className="missions-container">
+      <div className="line-1">
+        <h3 className="mname-case">Mission</h3>
+        <h3 className="mdesc-case">Description</h3>
+        <h3 className="mstat-case">Status</h3>
+        <h3 className="mbtn-case">Press</h3>
+      </div>
       {
         listOfMissions.map((item) => (
           <Mission
